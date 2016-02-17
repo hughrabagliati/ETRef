a0557754416a4f95e8cbb4ed18a259962b079242
hughrabagliati <hugh.rabagliati@gmail.com>
2016-02-17 16:37:23 +0000

    NewExpt5

---
+++ Expt5-TwoNamesET/ISI_analysis.r
@@ -0,0 +1,27 @@
+library(plyr)
+library(doBy)
+library(lme4)
+
+isi <- read.csv("./data/500ms_Ambig.csv")
+
+isi$foil_duration_R <- (isi$foil_duration_R - mean(isi$foil_duration_R))/sd(isi$foil_duration_R)
+isi$target_duration_R <- (isi$target_duration_R - mean(isi$target_duration_R))/sd(isi$target_duration_R)
+
+summary(glmer(informative_foil ~ foil_duration_R + (1|participant_number), data = isi, family = "binomial"))
+summary(glmer(informative_foil ~ target_duration_R + (1|participant_number), data = isi, family = "binomial"))
+
+
+resp <- read.csv("./data/responses_ambig_and_cont.csv")
+resp <- reshape(resp, 
+  varying = c("informative_target", "informative_foil"), 
+  v.names = "Informative",
+  timevar = "Response", 
+  times = c("target", "foil"),
+  direction = "long")
+
+resp$Response <- as.factor(resp$Response)
+contrasts(resp$Response)[1] <- -1
+summaryBy(Informative ~ Response  + condition, data = resp, na.rm = T)
+summary(glmer(Informative ~ condition * as.factor(Response) + (1+condition|Subject) + (1|Trial_ID), data = resp, family = "binomial"))
+# Note that model does not converge with response as a random effect for Subject or Trial_ID, nor with condition as random effect for
+# Trial_ID 
